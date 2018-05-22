4654911509dcc622ce7da1b62f69d953ecc71479
katarina <k.dabo@dtg.nl>
2018-05-22 17:31:09 +0200

    add http request

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var request = require('superagent');
+var path = require('path');
 
 var app = express();
 var public_path = express.static(__dirname + '/public');
@@ -15,9 +17,24 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/wines', function (req, res) {
+    var id = req.params.id;
+    res.header('Content-Type', 'application/json');
+    request
+        .get('https://wines-api.herokuapp.com/api/wines/' + id)
+        .end(function (err, response) {
+            res.send(response.text);
+            console.log(response.text);
+        });
+});
+
+app.get('/wines/:id', function (req, res) {
+
+});
+
 var server = app.listen(port, function () {
     const port = server.address().port;
-    console.info(__dirname + "==> Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+    console.info(__dirname + " ==> Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
 });
 
 module.exports = server;
